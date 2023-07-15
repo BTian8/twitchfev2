c6e39f03207f54f49858978552c25b94e1f3e0e1
Boyan Tian <>
2023-07-15 15:26:17 -0400

    update

---
+++ src/Child.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+const Child = (props) => {
+  const { msg, balance: parentBalance } = props;
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    console.log("componentDidUpdate/DidMount - Child Component");
+    return () => {
+      console.log("componentWillUnmount - Child Component");
+    };
+  });
+
+  console.log("render() - Child Component");
+  return (
+    <div>
+      <p>this is Child Component</p>
+      <p>Parent' balance: {parentBalance}</p>
+      <p>Parent' msg: {msg}</p>
+      <p>我的小金库：{balance}</p>
+
+      <div>
+        <button
+          onClick={() => {
+            setBalance(balance + 10);
+          }}
+        >
+          我自己挣钱了
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Child;
